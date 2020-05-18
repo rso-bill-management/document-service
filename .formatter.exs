66c08652b1d96ec2d33fef2cd40b7b0a2ada635a
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-05-19 00:11:12 +0200

    chore: add formatter

---
+++ .formatter.exs
@@ -0,0 +1,9 @@
+# Used by "mix format"
+[
+  inputs: [
+    "{mix,.formatter}.exs",
+    "{config,lib,test}/**/.{ex,exs}",
+    "apps/**/**/*.{ex,exs}",
+    "to_be_restored/**/**/*.{ex,exs}"
+  ]
+]
