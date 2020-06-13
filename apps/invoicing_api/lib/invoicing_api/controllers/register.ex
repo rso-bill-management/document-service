ef268d46c38f51644bf4a868e1994c255305f36c
Tomasz Kot <kottomasz98@gmail.com>
2020-06-13 16:59:23 +0200

    Update

--- apps/invoicing_api/lib/invoicing_api/controllers/register.ex
+++ apps/invoicing_api/lib/invoicing_api/controllers/register.ex
@@ -1,8 +1,18 @@
 defmodule InvoicingSystem.API.RegisterController do
     use InvoicingSystem.API, :controller
+    
+    alias InvoicingSystem.IAM.Users
   
-    def register(conn, %{"username" => username, "password" => password, "email" => email}) do
-        json_resp({:ok, %{status: :ok}}, conn)
+    def register(conn, %{"username" => username, "password" => password}) do
+        case Users.add(username: username, password: password) do
+            :ok -> 
+                conn
+                |> put_status(:created)
+                |> json(%{created: true})
+            {:error, arr} ->
+                conn
+                |> put_status(:conflict)
+                |> json(%{errors: arr})
+        end
     end
 end
-  
