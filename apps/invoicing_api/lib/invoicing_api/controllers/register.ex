455b503e50b76ebf9097d85950e418df3b6f8938
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-06-20 01:10:18 +0200

    chore: format code

--- apps/invoicing_api/lib/invoicing_api/controllers/register.ex
+++ apps/invoicing_api/lib/invoicing_api/controllers/register.ex
@@ -1,18 +1,19 @@
 defmodule InvoicingSystem.API.RegisterController do
-    use InvoicingSystem.API, :controller
-    
-    alias InvoicingSystem.IAM.Users
-  
-    def register(conn, %{"username" => username, "password" => password}) do
-        case Users.add(username: username, password: password) do
-            :ok -> 
-                conn
-                |> put_status(:created)
-                |> json(%{created: true})
-            {:error, arr} ->
-                conn
-                |> put_status(:conflict)
-                |> json(%{errors: arr})
-        end
+  use InvoicingSystem.API, :controller
+
+  alias InvoicingSystem.IAM.Users
+
+  def register(conn, %{"username" => username, "password" => password}) do
+    case Users.add(username: username, password: password) do
+      :ok ->
+        conn
+        |> put_status(:created)
+        |> json(%{created: true})
+
+      {:error, arr} ->
+        conn
+        |> put_status(:conflict)
+        |> json(%{errors: arr})
     end
+  end
 end
