37039cea95ba0ce4697dd6e03867ea422ceb80bd
Tomasz Kot <kottomasz98@gmail.com>
2020-06-19 11:37:28 +0200

    Not clean commit history

--- apps/invoicing_api/priv/repo/migrations/20200617100947_create_create_contractors.exs
+++ apps/invoicing_api/priv/repo/migrations/20200617100947_create_create_contractors.exs
@@ -2,8 +2,8 @@ defmodule InvoicingApi.Repo.Migrations.CreateCreateContractors do
   use Ecto.Migration
 
   def change do
-    create table(:create_contractors) do
-      add :id, :string
+    create table(:create_contractors, primary_key: false) do
+      add :id, :string, primary_key: true
       add :name, :string
       add :taxpayerIdentificationNumber, :string
       add :town, :string
