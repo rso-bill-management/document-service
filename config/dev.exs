a6ec0a883a917ff4398a289d74cc6a96d03e78d1
Tomasz Kot <kottomasz98@gmail.com>
2020-06-20 23:32:01 +0200

    changes

--- config/dev.exs
+++ config/dev.exs
@@ -4,16 +4,16 @@ config :joken,
   default_signer: {:system, "INVOICING_JOKEN_SECRET", "some_really_secret_secret"}
 
 config :invoicing_db, InvoicingSystem.DB.Repo,
-  database: "postgres",
-  username: "postgres", 
-  password: "postgres", 
-  hostname: "localhost",
-  port: 5432
-  # database: {:system, "PGDATABASE", "postgres"},
-  # username: {:system, "PGUSER", "postgres"},
-  # password: {:system, "PGPASSWORD", "postgres"},
-  # hostname: {:system, "PGHOST", "postgres"},
-  # port: {:system, "PGPORT", 5432, {String, :to_integer}}
+  # database: "postgres",
+  # username: "postgres", 
+  # password: "postgres", 
+  # hostname: "localhost",
+  # port: 5432
+  database: {:system, "PGDATABASE", "postgres"},
+  username: {:system, "PGUSER", "postgres"},
+  password: {:system, "PGPASSWORD", "postgres"},
+  hostname: {:system, "PGHOST", "postgres"},
+  port: {:system, "PGPORT", 5432, {String, :to_integer}}
 
 config :invoicing_storage,
   data_path: {:system, "INVOICING_STORAGE_DATA_PATH", "/tmp"}
