7eddcc2b369014de922b158fd49f2742d7a8ae96
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-06-23 13:36:06 +0200

    chore: formatter

--- apps/invoicing_api/lib/invoicing_api/controllers/invoice.ex
+++ apps/invoicing_api/lib/invoicing_api/controllers/invoice.ex
@@ -1,22 +1,22 @@
 defmodule InvoicingSystem.API.InvoiceController do
-    use InvoicingSystem.API, :controller
+  use InvoicingSystem.API, :controller
 
-    alias InvoicingSystem.API.Invoices
-    alias InvoicingSystem.API.Renderer
+  alias InvoicingSystem.API.Invoices
+  alias InvoicingSystem.API.Renderer
 
-    require Logger
+  require Logger
 
-    def show(conn, %{"uuid" => uuid}) do
-        with {:ok, invoice} <- Invoices.get_invoice(uuid),
-             {:ok, file_contents} <- Renderer.render(invoice) do
-            Logger.info("Downloading pdf file from backend")
-            send_download(conn, {:binary, file_contents},
-                filename: "#{invoice.number}_#{invoice.date_issue}.pdf"
-            )
-        else
-        error ->
-            json_resp({:not_found, %{error: :not_found}}, conn)
-        end
+  def show(conn, %{"uuid" => uuid}) do
+    with {:ok, invoice} <- Invoices.get_invoice(uuid),
+         {:ok, file_contents} <- Renderer.render(invoice) do
+      Logger.info("Downloading pdf file from backend")
+
+      send_download(conn, {:binary, file_contents},
+        filename: "#{invoice.number}_#{invoice.date_issue}.pdf"
+      )
+    else
+      error ->
+        json_resp({:not_found, %{error: :not_found}}, conn)
     end
+  end
 end
-  
